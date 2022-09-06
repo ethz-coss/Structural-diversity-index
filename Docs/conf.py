b346d6ddd90cc57da7c099eb4513161d99a09cb1
andreamusso96 <38622483+andreamusso96@users.noreply.github.com>
2022-09-06 14:14:52 +0200

    more docs update

--- Docs/conf.py
+++ Docs/conf.py
@@ -11,7 +11,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../PythonPackage/src/'))
+sys.path.insert(0, os.path.abspath('../PythonPackage/src'))
+sys.path.insert(0, os.path.abspath('../PythonPackage/src/structural_diversity_index'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
--- Docs/modules.rst
+++ Docs/modules.rst
@@ -2,6 +2,6 @@ structural_diversity_index
 ==========================
 
 .. toctree::
-   :maxdepth: 4
+   :maxdepth: 5
 
    structural_diversity_index
